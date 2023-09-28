592bb13f168cc1dfa207cd81c7b2e97973aecb9c
LeonardoAugustoVC <135409863+LeonardoAugustoVC@users.noreply.github.com>
2023-09-28 13:00:51 -0600

    Add files via upload
    Anexo archivos sobre los algoritmos de regresión lineal siendo aplicados en un dataset con datos de Biomecanica de la columna.  Pueden leer el archivo en PDF.
    Annex files on the linear regression algorithms being applied to a dataset with spine biomechanics data. You can read the PDF file.

---
+++ LIBRERIAS.py
@@ -0,0 +1,24 @@
+# Librerías Báscias
+
+import pandas as pd  
+import numpy as np
+import matplotlib.pyplot as plt  
+import plotly.express as px
+
+
+
+# Librerías para Regresiones 
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import cross_val_score
+
+import statsmodels.api as sm  
+
+# Generar Datos Sintéticos
+
+from sklearn.datasets import make_classification
+
+from sklearn.datasets import make_regression
+
+import random
